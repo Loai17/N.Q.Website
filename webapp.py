5d8d17516b34d812615ac4114cbf7000b935a485
Loai17 <low.eyed.studios@gmail.com>
2018-03-12 16:03:16 +0200

    Update Tabs and spaces

--- webapp.py
+++ webapp.py
@@ -37,27 +37,27 @@ DBSession = sessionmaker(bind=engine, autoflush=False)
 session = DBSession()
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET','POST'])
 def home():
 	form = ContactForm()
 
 	if request.method == 'POST':
-	    if form.validate() == False:
-	      	flash('All fields are required.')
-	      	return render_template('index.html', form=form)
-	    else:
-	    	msg = Message("Website Message", sender='contactUs@example.com', recipients=['loai.qubti@gmail.com'])
-      		msg.body = """
-From: %s <%s>
-Subject: %s
-Message: %s
-      		""" % (form.name.data, form.email.data, form.subject.data, form.message.data)
-      		mail.send(msg)
-	      	
-	      	return render_template('index.html', success=True)
+		if form.validate() == False:
+			flash('All fields are required.')
+			return render_template('index.html', form=form)
+		else:
+			msg = Message("Website Message", sender='contactUs@example.com', recipients=['loai.qubti@gmail.com'])
+			msg.body = """
+			From: %s <%s>
+			Subject: %s
+			Message: %s
+			""" % (form.name.data, form.email.data, form.subject.data, form.message.data)
+			mail.send(msg)
+			
+			return render_template('index.html', success=True)
 		
 	return render_template('index.html', form=form)
 
